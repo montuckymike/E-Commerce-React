9311704f0f2a78fa5ee39f8bc907823981e30381
montuckymike <mike@kosmojo.com>
2017-08-17 11:36:15 -0600

    added tests routes and models

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const productRoutes = require('./routes/products')
 
-const app = express();
+const app = express()
 
-const port = 3001;
+const port = 3001
 
-const server = app.listen(port, () => console.log(`listening on port ${port}`));
+mongoose.connect('mongodb://localhost/shopping-cart')
 
-module.exports = server;
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/api/products', productRoutes)
+
+const server = app.listen(port, () => console.log(`listening on port ${port}`))
+
+module.exports = server
