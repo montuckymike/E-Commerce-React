f70678a0f0f6d5d1a98ef27e55173ea2d219c456
montuckymike <mike@kosmojo.com>
2017-08-18 13:11:23 -0600

    updated routes and test for deleting products

--- test/product.test.js
+++ test/product.test.js
@@ -28,4 +28,85 @@ describe('Products', () => {
     })
   })
 
+  describe('/POST product', () => {
+    it('it should not POST products without all fields', done => {
+      const product = { name: 'shoe' }
+      chai.request(server)
+        .post('/api/products')
+        .send(product)
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('object')
+          res.body.message.should.have.property('errors')
+          res.body.should.have.property('data')
+          res.body.should.have.property('data').eql(null)
+          done()
+      })
+  })
+
+    it('it should POST a product with all fields', done => {
+      const product = new Product({
+        name: 'shoe lace',
+        price: 19.93,
+        image: 'na'
+      })
+      chai.request(server)
+        .post('/api/products')
+        .send(product)
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('object')
+          res.body.message.length.should.be.above(0)
+          res.body.should.have.property('data')
+          res.body.data.should.be.a('object')
+          done()
+        })
+      })
+    })
+
+    describe('/GET/:product_id', () => {
+      it('it should GET one product with all the fields', done => {
+        const product = new Product({
+          name: 'an item',
+          price: 500.15,
+          image: 'na'
+        })
+        product.save((err, product) => {
+          chai.request(server)
+          .get(`/api/products/${product._id}`)
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            res.body.message.length.should.be.above(0)
+            res.body.should.have.property('data')
+            res.body.data.should.be.a('object')
+            done()
+          })
+        })
+
+      })
+    })
+
+    describe('/DELETE/:product_id product', () => {
+      it('it should DELETE a product', done => {
+        const product = new Product({
+          name: 'Pivot Switchblade',
+          price: 8999.99,
+          image: 'pic of sick whip'
+        })
+        product.save((err, product) => {
+          chai.request(server)
+          .delete(`/api/products/${product._id}`)
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            res.body.message.length.should.be.above(0)
+            res.body.should.have.property('data')
+            res.body.data.should.eql({})
+            done()
+          })
+        })
+      })
+    })
+
 })
